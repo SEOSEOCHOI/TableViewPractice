c0da492d455876b27221f9a8287a84386486b8be
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-01-06 20:03:05 +0900

    ChattingList

---
+++ TableViewPractice/ChattingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingTableViewCell.swift
+//  TableViewPractice
+//
+//  Created by 최서경 on 1/6/24.
+//
+
+import UIKit
+
+class ChattingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
