2004e3388eb5ca548381a51b9b5318d588d0b1f8
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-01-08 20:09:27 +0900

    여행 매거진 화면 만들기 [Struct, Kingfisher, Date]

---
+++ TableViewPractice/SeSACTravelTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SeSACTravelTableViewCell.swift
+//  TableViewPractice
+//
+//  Created by 최서경 on 1/8/24.
+//
+
+import UIKit
+
+class SeSACTravelTableViewCell: UITableViewCell {
+
+    @IBOutlet var photoImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+}
