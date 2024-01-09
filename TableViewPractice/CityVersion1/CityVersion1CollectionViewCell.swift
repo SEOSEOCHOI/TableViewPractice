8f2e5ea8701554d425e17f89298962187318884f
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-01-09 19:26:02 +0900

    세그먼트 없이 컬렉션 뷰로만 구성히기

---
+++ TableViewPractice/CityVersion1/CityVersion1CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CityVersion1CollectionViewCell.swift
+//  TableViewPractice
+//
+//  Created by 최서경 on 1/9/24.
+//
+
+import UIKit
+
+class CityVersion1CollectionViewCell: UICollectionViewCell {
+    @IBOutlet var cityImageView: UIImageView!
+    @IBOutlet var cityEnglishNameLabel: UILabel!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var cityExplainLabel: UILabel!
+    
+}
--- TableViewPractice/SeSACTravelTableViewCell.swift
+++ TableViewPractice/SeSACTravelTableViewCell.swift
@@ -1,16 +1,10 @@
-//
-//  SeSACTravelTableViewCell.swift
-//  TableViewPractice
-//
-//  Created by 최서경 on 1/8/24.
-//
-
 import UIKit
 
 class SeSACTravelTableViewCell: UITableViewCell {
 
     @IBOutlet var photoImage: UIImageView!
     @IBOutlet var titleLabel: UILabel!
+    
     @IBOutlet var subtitleLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
     
